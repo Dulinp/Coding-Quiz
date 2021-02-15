25023fa745af4bf6aada2eb7d4bc62b4cca5f8ff
Peter.Dulin <petertdulin@gmail.com>
2021-02-15 14:31:24 -0500

    fixing major functionality issues

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -3,12 +3,19 @@ var highScores = document.querySelector("#high-scores");
 var timerElm = document.querySelector("#time");
 var startButton = document.querySelector("#start-button");
 var startScreen = document.querySelector("#starting-screen");
+var questionsDiv = document.querySelector("#questions");
 var questionTitle = document.getElementById("question-title");
-var questionChoices = document.getElementById("choices")
+var questionChoices = document.getElementById("choices");
+var rightWrong = document.getElementById("right-wrong");
+var answerOptions = document.querySelectorAll(".answer-option");
+var option1 = document.getElementById("option1");
+var option2 = document.getElementById("option2");
+var option3 = document.getElementById("option3");
+var option4 = document.getElementById("option4");
 //Set other variables
 var secondsLeft = 75;
 var currentQuestion = 0;
-var score = 0;
+
 
 //Load local storage of previous scores
 
@@ -22,12 +29,12 @@ var score = 0;
 //Timer Funcgtion
 function setTimer() {
     var timerInterval = setInterval(function () {
+        if (secondsLeft !== 0) {
         secondsLeft--;
         timerElm.textContent = "time: " + secondsLeft;
 
-        if (secondsLeft === 0) {
+        }else {secondsLeft === 0
             clearInterval(timerInterval);
-            gameOver();
 
         }
     }, 1000);
@@ -35,37 +42,68 @@ function setTimer() {
 }
 
 //Target start button on click to hide, start quiz, and start timer
-$(startButton).on("click", function () {
-    $(startScreen).hide();
+startButton.addEventListener("click", function () {
+    startScreen.style.display = "none";
     setTimer();
-})
+});
 
 //Timer countdown starts
 
-//Function for questions
+//Function for presenting questions
 function questionStart() {
-   if (currentQuestion < questions.length) {
-       questionTitle.textContent = questions[currentQuestion].title;
-       questionChoices.textContent = questions[currentQuestion].choices;
+    if (currentQuestion < questions.length) {
+        questionTitle.textContent = questions[currentQuestion].title;
 
-       for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-         questionChoices = document.createElement("choices");  
-       }
-} 
-}  
-        
-   
+        for (var i = 0; i < questions[currentQuestion].choices.length; i++) {
+            answerOptions[i].textContent = questions[currentQuestion].choices[i];
+            questionChoices.setAttribute("data-id", i);
+        }
 
-    //Array of questions as objects
+    } else {questionsDiv.style.display = "none";
+    }
+}
 
-    //create function to render the questions
+//Function to go to the next question
+function nextQuestion() {
+    currentQuestion++;
+}
+
+//Function for user to select answer, if wrong deducting time, and going through the questions
+function answerOptionSelected(event) {
+    event.stopPropagation();
+
+    if (this.textContent === questions[currentQuestion].answer) {
+        rightWrong.textContent = "Correct!";
+        nextQuestion();
+        questionStart();
+
+    }else {this.textContent !== questions[currentQuestion].answer
+        rightWrong.textContent = "Wrong!";
+        secondsLeft = (secondsLeft - 15);
+        nextQuestion();
+        questionStart();
+    }
+}
 
 
-//For answering a question
-    //add event listener for click right answer- this leads to alert that is correct and goes to next question
+    //Event listeners for each answer option
+    option1.addEventListener("click", answerOptionSelected);
+    option2.addEventListener("click", answerOptionSelected);
+    option3.addEventListener("click", answerOptionSelected);
+    option4.addEventListener("click", answerOptionSelected);
 
-    //if incorrect-this leads to the alert for answer is incorrect and goes to the next question
+    //Function for game ending for winner
+    // function gameOver() {
+    //     if(currentQuestion > questions.length{
+    //     questionsDiv.style.display = "none";
 
-    //create a function that decreases time by 15 seconds each time an incorrect answer is clicked.
+    //     }else {
 
-//add function for game over if time runs out before 10 questions are answered
+    //     }
+
+
+    // }
+
+
+
+
